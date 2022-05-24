4d9189fd0cfef746159605d82eacebce9f6554ab
dd-code-immersives <domilamad89@gmail.com>
2022-05-24 21:18:27 +0300

    added divisors, is_even and boolean tests

--- calculator_tests.py
+++ calculator_tests.py
@@ -2,6 +2,7 @@ import unittest
 from calculator import Calculator
 
 
+# the class is the "test suite"
 class TestCalculatorMethods(unittest.TestCase):
 
     def setUp(self):
@@ -9,7 +10,13 @@ class TestCalculatorMethods(unittest.TestCase):
 
     def test_add(self):
 
-        self.assertEqual(self.calc.add(3,3), 6, "answer should be 6")
+        self.assertEqual(self.calc.add(3,3), 6, "tests addition of two arguments on integers")
+        self.assertEqual(self.calc.add(3,3,3), 9, "tests addition of three arguments on integers")
+        self.assertEqual(self.calc.add(3.2,3.2), 6.4, "tests addition on floats")
+        self.assertEqual(self.calc.add(-3, -3), -6, "tests adding negatives")
+        #self.assertEqual(self.calc.add("ab", "cd"),  "tests adding negatives")
+        # with self.assertRaises(TypeError):
+        # 	pass
 
     def test_sub(self):
 
@@ -19,6 +26,19 @@ class TestCalculatorMethods(unittest.TestCase):
 
         self.assertEqual(self.calc.multi(3,3), 9, "answer should be nine")
 
+    def test_division(self):
+    	self.assertEqual(self.calc.division(3,3), 1, "answer should be 1")
+    	self.assertIsNone(self.calc.division(3,0), "Division by zero impossible") 
+
+    def test_is_even(self):
+    	self.assertTrue(self.calc.is_even(0))
+    	self.assertFalse(self.calc.is_even(3))
+    	self.assertTrue(self.calc.is_even(4))
+
+
+    def test_divisors(self):
+    	self.assertIn(3, self.calc.divisors(9))
+    	self.assertIn(9, self.calc.divisors(9))
 
 if __name__ == '__main__':
     unittest.main()
